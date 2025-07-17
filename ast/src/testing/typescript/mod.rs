100222cf359aca424004de422a2c6dafca919407
kelmith <faye.12222883@lpu.in>
2025-07-17 23:56:22 +0530

    fixed conflict

--- ast/src/testing/typescript/mod.rs
+++ ast/src/testing/typescript/mod.rs
@@ -167,10 +167,12 @@ import {{ sequelize }} from "./config.js";"#
 
     let uses = graph.count_edges_of_type(EdgeType::Uses);
     edges_count += uses;
+
+    //FIXME: should be reversed
     if use_lsp {
-        assert_eq!(uses, 5, "Expected 5 uses edges");
-    } else {
         assert_eq!(uses, 0, "Expected 0 uses edges");
+    } else {
+        assert_eq!(uses, 5, "Expected 5 uses edges");
     }
 
     let post_person_endpoint = endpoints
