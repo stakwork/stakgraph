a53f413b97b288fd02e258409b70ea9eb25baffb
kelmith <faye.12222883@lpu.in>
2025-10-05 07:50:21 +0530

    fix test

--- ast/src/testing/ruby/mod.rs
+++ ast/src/testing/ruby/mod.rs
@@ -266,9 +266,9 @@ pub async fn test_ruby_generic<G: Graph>() -> Result<()> {
     edges_count += calls;
 
     if use_lsp {
-        assert_eq!(calls, 27, "Expected 27 call edges with lsp");
+        assert_eq!(calls, 30, "Expected 30 call edges with lsp");
     } else {
-        assert_eq!(calls, 14, "Expected 14 call edges");
+        assert_eq!(calls, 17, "Expected 17 call edges");
     }
 
     let uses = graph.count_edges_of_type(EdgeType::Uses);
