11f3cf7e2dc36a938181acecc8240e1270d35dd0
kelmith <faye.12222883@lpu.in>
2025-11-25 22:14:37 +0530

    feat: capture endpoint object identifier to correctly resolve grouped router endpoints

--- ast/src/lang/graphs/neo4j_graph.rs
+++ ast/src/lang/graphs/neo4j_graph.rs
@@ -1103,49 +1103,40 @@ impl Neo4jGraph {
     pub(super) async fn process_endpoint_groups_async(
         &self,
         eg: Vec<NodeData>,
-        lang: &Lang,
+        _lang: &Lang,
     ) -> Result<()> {
-        let mut groups_with_endpoints = Vec::new();
+        if eg.is_empty() {
+            return Ok(());
+        }
+
+        let connection = self.ensure_connected().await?;
+        let mut txn_manager = TransactionManager::new(&connection);
 
         for group in &eg {
-            if let Some(group_function_name) = group.meta.get("group") {
-                let group_functions = self
-                    .find_nodes_by_name_async(NodeType::Function, group_function_name)
-                    .await;
+            if let Some(router_var_name) = group.meta.get("group") {
+                let prefix = &group.name;
+                let group_file = &group.file;
 
-                if let Some(group_function) = group_functions.first() {
-                    let mut all_endpoints = Vec::new();
+                let query = format!(
+                    "MATCH (e:Endpoint) 
+                     WHERE e.file = $file 
+                       AND NOT e.name STARTS WITH $prefix
+                       AND NOT e.name CONTAINS '/:' 
+                       AND e.object = $object
+                     SET e.name = $prefix + e.name
+                     RETURN e.name as updated_name"
+                );
 
-                    for finder_query in lang.lang().endpoint_finders() {
-                        if let Ok(endpoints) = lang.get_query_opt::<Self>(
-                            Some(finder_query),
-                            &group_function.body,
-                            &group_function.file,
-                            NodeType::Endpoint,
-                        ) {
-                            all_endpoints.extend(endpoints);
-                        }
-                    }
+                let mut params = BoltMap::new();
+                boltmap_insert_str(&mut params, "file", group_file);
+                boltmap_insert_str(&mut params, "prefix", prefix);
+                boltmap_insert_str(&mut params, "object", router_var_name);
 
-                    if !all_endpoints.is_empty() {
-                        groups_with_endpoints.push((group.clone(), all_endpoints));
-                    }
-                }
+                txn_manager.add_query((query, params));
             }
         }
 
-        if !groups_with_endpoints.is_empty() {
-            let connection = self.ensure_connected().await?;
-            let mut txn_manager = TransactionManager::new(&connection);
-
-            let queries = process_endpoint_groups_queries(&groups_with_endpoints);
-            for query in queries {
-                txn_manager.add_query(query);
-            }
-
-            txn_manager.execute().await?;
-        }
-
+        txn_manager.execute().await?;
         Ok(())
     }
 
