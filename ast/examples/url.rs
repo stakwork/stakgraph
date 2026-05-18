35f35803ef37672b74f67ccd4f7f7f911509361d
Faye Kelmith <faye.12222883@lpu.in>
2026-05-18 21:45:38 +0530

    chore: clean up examples (#1272)

--- ast/examples/url.rs
+++ ast/examples/url.rs
@@ -3,16 +3,26 @@ use ast::utils::{logger, print_json};
 use ast::{self, repo::Repo};
 
 /*
-REV=a858f95a6e89b0ff8e7f1797685b73e126e4c5ae URL=https://github.com/stakwork/sphinx-tribes.git cargo run --example url
+URL=https://github.com/stakwork/sphinx-tribes.git cargo run --example url
+URL=https://github.com/pypa/sampleproject.git    cargo run --example url
+URL=https://github.com/rails/rails.git           cargo run --example url
+
+# with a specific revision:
+URL=https://github.com/stakwork/sphinx-tribes.git REV=<sha> cargo run --example url
+
+# with LSP-based cross-file linking:
+USE_LSP=true URL=https://github.com/stakwork/sphinx-tribes.git cargo run --example url
 */
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
     logger();
-    let url = std::env::var("URL").expect("URL is not set");
+
+    let url = std::env::var("URL").expect("URL env var is required");
     let use_lsp = std::env::var("USE_LSP").ok().map(|v| v == "true");
-    let rev = env_not_empty("REV");
-    let revs = rev
+    let revs = std::env::var("REV")
+        .ok()
+        .filter(|v| !v.is_empty())
         .map(|r| r.split(',').map(|s| s.to_string()).collect())
         .unwrap_or_default();
 
@@ -20,16 +30,8 @@ async fn main() -> Result<()> {
         Repo::new_clone_multi_detect(&url, None, None, Vec::new(), revs, None, None, use_lsp)
             .await?;
     let graph = repos.build_graphs().await?;
-    println!(
-        "Final Graph => {} nodes and {} edges",
-        graph.nodes.len(),
-        graph.edges.len()
-    );
+
+    println!("nodes: {}, edges: {}", graph.nodes.len(), graph.edges.len());
     print_json(&graph, "url")?;
     Ok(())
-}
-
-fn env_not_empty(name: &str) -> Option<String> {
-    // return None if it doesn't exist or is empty string
-    std::env::var(name).ok().filter(|v| !v.is_empty())
 }
